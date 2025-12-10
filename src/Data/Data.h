018251383c88f399a1bd6d75086af3c060c4904c
MartinJdy <jeudy.martin14@gmail.com>
2025-12-10 14:03:12 +0000

    Update data structure

--- src/Data/Data.h
+++ src/Data/Data.h
@@ -1,3 +1,6 @@
+#ifndef DATA_H
+#define DATA_H
+
 typedef enum {
     SOURCE,
     SPRING,
@@ -21,12 +24,71 @@ typedef enum {
 } FacilityType;
 
 typedef struct {
-    FacilityType facility;
-    char* facilityID;
     FacilityType upstream;
     char* upstreamID;
+
+    FacilityType facility;
+    char* facilityID;
+    
+    int annualAmount;
+    float leakPercentage;
+} Capture;
+
+ typedef struct {
+    FacilityType facility;
+    char* facilityID;
+    
+    int annualAmount;
+} Factory;
+
+ typedef struct {
+    FacilityType facility;
+    char* facilityID;
+
     FacilityType downstream;
     char* downstreamID;
-    float annualAmount;
+
     float leakPercentage;
-} Water;
+} Storage;
+
+ typedef struct {
+    FacilityType facility;        //usine qui a traité l'eau de ce tronçon
+    char* facilityID;
+
+    FacilityType downstream;
+    char* downstreamID;
+
+    FacilityType junction;
+    char* junctionID;
+
+    float leakPercentage;
+} principalDistribution;
+
+ typedef struct {
+    FacilityType facility;        //usine qui a traité l'eau de ce tronçon
+    char* facilityID;
+
+    FacilityType downstream;
+    char* downstreamID;
+
+    FacilityType junction;
+    char* junctionID;
+
+    float leakPercentage;
+} secondaryDistribution;
+
+
+typedef struct {
+    FacilityType facility;        //usine qui a traité l'eau de ce tronçon
+    char* facilityID;
+
+    FacilityType downstream;
+    char* downstreamID;
+
+    FacilityType junction;
+    char* junctionID;
+
+    float leakPercentage;
+} connexion;
+
+#endif
