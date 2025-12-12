25e26c72f90d78def7528d1daf42eb94e48bb063
SayfSbai60 <sayf.sbai.contact@gmail.com>
2025-12-12 16:27:04 +0100

    Update Data.h

--- src/Data/Data.h
+++ src/Data/Data.h
@@ -25,13 +25,22 @@ typedef enum {
     TYPE_UNKNOWN ,
 } FacilityType;
 
-/*typedef struct Facility {
-    FacilityType type;   
-    char id[50];         // identifiant unique
-    char parent_id[50];  // id de l'acteur amont
-    double volume;      
-    double leak;         
-} Facility;*/
+typedef enum {
+    UNKNOWN,
+    SOURCE_TO_FACTORY,
+    FACTORY_ONLY,
+    FACTORY_TO_STORAGE,
+    STORAGE_TO_JUNCTION,
+    JUNCTION_TO_SERVICE,
+    SERVICE_TO_CUST
+} LineType;
+
+typedef struct Resultat_Histo{
+    char* id;
+    double capacite_max; 
+    double volume_traite;
+    double volume_capte;
+}Resultat_Histo;
 
 typedef struct Facility{
     FacilityType type;
@@ -49,23 +58,6 @@ typedef struct Facility{
 
     int hauteur;
 }Facility;
-
-typedef enum {
-    UNKNOWN,
-    SOURCE_TO_FACTORY,
-    FACTORY_ONLY,
-    FACTORY_TO_STORAGE,
-    STORAGE_TO_JUNCTION,
-    JUNCTION_TO_SERVICE,
-    SERVICE_TO_CUST
-} LineType;
-
-typedef struct Resultat_Histo{
-    char* id;
-    double capacite_max; 
-    double volume_traite;
-    double volume_capte;
-}Resultat_Histo;
 
 
 
