58326c6692d1d87ed6f3559f7f86b9ee9af5348b
Etienne C. <59794011+etiennec78@users.noreply.github.com>
2025-12-12 17:39:26 +0100

    Update bash script
    * Simplify some tests
    * Add new tests
    * Take the database file as an argument

--- cwildwater.sh
+++ cwildwater.sh
@@ -1,91 +1,101 @@
-if [ $# -lt 1 ]; then
-    echo "Erreur: Pas assez d'arguments !"
-    exit 1
-fi
-
 help_message="""
 C-WildWaters: Un script pour effectuer un histogramme
 des usines de traitement d'eau à partir d'un fichier .dat.
 
+Utilisation: ./cwildwater.sh <db_file> [arg] [subarg]
+
 Arguments:
-    histo [arg]: Créé un histogramme à partir des arguments suivants:
+    histo [subarg]: Créé un histogramme à partir des sous-arguments suivants:
         max: Volume maximal de traitement de l'usine
         src: Volume total capté par les sources
         real: Volume total réellement traité
+
     leaks <id_usine>:
         Effectue la mesure des pertes d'une usine sur l'ensemble de son réseau aval.
         L'identifiant d'usine doit être complet et respecter la casse.
+
     help:
         Afficher ce message
 """
 
-# Check that the first argument is valid
-case "$1" in ("--help"|"help"|"histo"|"leaks")
+# Print the help message if requested
+for arg in $@; do
+    case "$arg" in ("--help"|"help"|"-h")
+            echo "$help_message"
+            exit 0
+        ;;
+    esac
+done
+
+# Check that enough args were given
+if [ $# -ne 3 ]; then
+    echo "$help_message"
+    echo "Erreur: Vous n'avez pas entré le nombre correct d'arguments !"
+    exit 1
+fi
+
+# Check that the database file exists
+if [ ! -f "$1" ]; then
+    echo "Erreur: Le fichier de données '$1' n'existe pas !"
+    exit 2
+fi
+
+# Check that the second argument is valid
+case "$2" in ("histo"|"leaks")
     ;;
     *)
         echo "$help_message"
-        echo "Erreur: Votre premier argument n'est pas valide ! "
-        exit 2
+        echo "Erreur: Votre argument '$2' n'est pas valide ! "
+        exit 3
     ;;
 esac
 
-# Print the help message if requested
-for arg in $@; do
-    if [[ "$arg" == "--help" || "arg" == "help" ]]; then
-        echo "$help_message"
-        exit 0
-    fi
-done
+if [ "$2" = "histo" ]; then
+    case "$3" in ("max"|"src"|"real")
+        ;;
+        *)
+            echo "$help_message"
+            echo "Erreur: L'argument '$3' est invalide pour '$2' ! "
+            exit 4
+        ;;
+    esac
+fi
+
+# Check that the factory exists in the file
+if [[ "$2" = "leaks" ]] && ! grep -q "$3" "$1"; then
+    echo "Erreur: L'id d'usine '$3' n'a pas été trouvé dans le fichier $1";
+    exit 5
+fi
 
 # Check that the makefile is present
 if [ ! -f Makefile ]; then
     echo "Erreur: Impossible de trouver le makefile !"
-    exit 3
+    exit 6
 fi
 
 # Check that the src dir is present
 if [ ! -d src ]; then
     echo "Erreur: Impossible de trouver les fichiers à compiler !"
-    exit 4
-fi
-
-# Check that the number of arguments is correct
-if [[ $# -ne 2 && ( "$1" == "histo" || "$1" == "leaks" ) ]]; then
-    echo "$help_message"
-    echo "Erreur: Vous n'avez pas entré le nombre correct d'arguments pour '$1'"
-    exit 5
-fi
-
-if [ "$1" = "histo" ]; then
-    case "$2" in ("max"|"src"|"real")
-        ;;
-        *)
-            echo "$help_message"
-            echo "Erreur: L'argument '$2' est invalide pour '$1' ! "
-            exit 6
-        ;;
-    esac
+    exit 7
 fi
 
 make
 
 if [ ! -f bin/cwildwater ]; then
     echo "Erreur: La compilation a échoué !"
-    exit 7
+    exit 8
 fi
 
 chmod +x bin/cwildwater
 
-rights=`ls -lh cwildwater.sh | cut -d " " -f 1`
-if [ ${rights: -1} != "x" ]; then
+if [ ! -x cwildwater.sh ]; then
     echo "Erreur: Le fichier binaire n'a pas pu être rendu exécutable !"
 fi
 
 ./bin/cwildwater $@
 make clean
 
-
 if [ -f bin/cwildwater ]; then
     echo "Erreur: Le nettoyage a échoué !"
-    exit 8
+    exit 9
 fi
