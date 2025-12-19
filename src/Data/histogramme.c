ed2740c69c91b429eab84a3eb28441cc214d6fd4
Etienne C. <59794011+etiennec78@users.noreply.github.com>
2025-12-19 20:48:19 +0100

    Remove unused function

--- src/Data/histogramme.c
+++ src/Data/histogramme.c
@@ -46,36 +46,3 @@ Resultat_Histo* recup_histo_data(Facility* node, char* id_search){
 
     return data;
 }
-
-void parcours_inverse_et_ecriture(Facility* node, FILE* fichier, const char* type_metrique) {
-    if (node != NULL) {
-       
-        parcours_inverse_et_ecriture(node->droite, fichier, type_metrique);
-
-        
-        double volume_km3 = 0;
-        if (strcmp(type_metrique, "max") == 0) {
-            volume_km3 = node->capacite_max;
-        } else if (strcmp(type_metrique, "src") == 0) {
-            volume_km3 = node->volume_capte;
-        } else if (strcmp(type_metrique, "real") == 0) {
-            volume_km3 = node->volume_traite;
-        }
-
-        
-        double volume_mm3 = (double)volume_km3 / 1000.0;
-
-        fprintf(fichier, "%s;%.3f\n", node->id, volume_mm3); 
-
-        parcours_inverse_et_ecriture(node->gauche, fichier, type_metrique);
-    }
-}
-
-
-
-
-
-
-
-
-
