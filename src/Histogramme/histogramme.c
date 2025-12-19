94709cca3b30a58a47bc7ad8f5a1b78f173e3d5e
Etienne C. <59794011+etiennec78@users.noreply.github.com>
2025-12-19 11:41:53 +0100

    Handle all other histo arguments in the c code

--- src/Histogramme/histogramme.c
+++ src/Histogramme/histogramme.c
@@ -1,36 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "histogramme.h"
 #include "../Data/usine_avl.h"
 
-void print_avl_reverse(Facility* node, FILE* f) {
+
+double get_capacite_max(Facility* f) {
+    return f->capacite_max;
+}
+
+double get_volume_capte(Facility* f) {
+    return f->volume_capte;
+}
+
+double get_volume_traite(Facility* f) {
+    return f->volume_traite;
+}
+
+void print_avl_reverse(Facility* node, FILE* f, capacity_getter getter) {
     if (node == NULL) return;
-    print_avl_reverse(node->droite, f);
-    fprintf(f, "%s;%f\n", node->id, node->capacite_max);
-    print_avl_reverse(node->gauche, f);
+    print_avl_reverse(node->droite, f, getter);
+    fprintf(f, "%s;%f\n", node->id, getter(node));
+    print_avl_reverse(node->gauche, f, getter);
 }
 
 void histogramme(char* db_path, char* histo_type) {
-
     Facility* avl;
-    if (strcmp(histo_type, "max") == 0) {
-        avl = creerAVLMax(db_path);
-        if (avl == NULL) {
-            fprintf(stderr, "Erreur: Impossible de créer l'arbre AVL (fichier vide ou mal formé)\n");
-            return;
-        }
-        
-        FILE* f = fopen("data/vol_max.dat", "w");
-        if (f == NULL) {
-            perror("Erreur création fichier vol_max.dat");
-            free_avl_usine(avl);
-            return;
-        }
-        
-        fprintf(f, "Usine;Capacite\n");
-        print_avl_reverse(avl, f);
-        
-        fclose(f);
-        free_avl_usine(avl);
+    char* filePath;
+    capacity_getter getter;
+    char* dataTitle;
+
+    avl = creerAVLMax(db_path);
+    if (avl == NULL) {
+        fprintf(stderr, "Erreur: Impossible de créer l'arbre AVL (fichier vide ou mal formé)\n");
+        return;
     }
+
+    if (strcmp(histo_type, "max") == 0) {
+        filePath = "data/vol_max.dat";
+        getter = get_capacite_max;
+        dataTitle = "Capacité maximale";
+    }
+    else if (strcmp(histo_type, "src") == 0) {
+        filePath = "data/vol_captation.dat";
+        getter = get_volume_capte;
+        dataTitle = "Volume capté";
+    }
+    else {
+        filePath = "data/vol_traitement.dat";
+        getter = get_volume_traite;
+        dataTitle = "Volume traité";
+    }
+
+    FILE* f = fopen(filePath, "w");
+    if (f == NULL) {
+        perror("Erreur création fichier de sortie");
+        free_avl_usine(avl);
+        return;
+    }
+
+    char title[50];
+    strcat(title, "Usine");
+    strcat(title, dataTitle);
+    strcat(title, "\n");
+    fprintf(f, "%s", title);
+    print_avl_reverse(avl, f, getter);
+
+    fclose(f);
+    free_avl_usine(avl);
 }
--- src/Histogramme/histogramme.h
+++ src/Histogramme/histogramme.h
@@ -1,1 +1,5 @@
+#include "../Data/Data.h"
+
+typedef double (*capacity_getter)(Facility* f);
+
 void histogramme(char* db_path, char* histo_type);
