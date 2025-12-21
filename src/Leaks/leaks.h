6953f494ba70c96dfd32dfb74fcdbbfae6a3dd73
SayfSbai60 <sayf.sbai.contact@gmail.com>
2025-12-21 15:25:35 +0100

    Cleanup: Remove unused functions in leaks.h
    Removed unused function declarations from leaks.h

--- src/Leaks/leaks.h
+++ src/Leaks/leaks.h
@@ -4,10 +4,5 @@
 #include "../Data/Data.h"
 
 void leaks(char* db_path, char* target_id);
-void freeAll(NodeIndex* root);
-NetworkComponent* find_in_index(NodeIndex* root, char* id);
-NodeIndex* insert_index(NodeIndex** root, NetworkComponent* comp);
-NetworkComponent* get_or_create(NodeIndex** index, char* id);
-double calculate_recursive_volume(NetworkComponent* current, double volume_in);
 
 #endif
